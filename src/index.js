3034b407d0bf7839b091b8ff9d84ce096959e86d
sleepy0owl <souravcristiano502@gmail.com>
2021-08-26 16:30:03 +0530

    initial commit

---
+++ src/index.js
@@ -0,0 +1,102 @@
+const aws = require('aws-cdk');
+aws.config.update({region:"us-east-1"})
+async function create_shorturl(body, domain, path){
+    let result = {};
+    try {
+        const dynamodb = new aws.Dynamodb.DocumentClient()
+        let putparam = {
+            Tablename: process.env.TABLE_NAME,
+            Item: {
+                "id": '',
+                "long_url": ''
+            }
+        };
+        let putresult = await dynamodb.put(putparam).promise();
+        console.log(putresult);
+        let shorturl = `https://${domain}/${path}/${id}`;
+        result = {
+            success: true,
+            shorturl: shorturl
+        }
+    } catch (error) {
+        console.error(error);
+        result = {
+            success: false,
+            message: 'something went wrong'
+        }
+    }
+    return result
+}
+
+async function retrieve_long_url(param) {
+    let result = {};
+    try {
+        const dynamodb = new aws.Dynamodb.DocumentClient();
+        let getparam = {
+            Tablename: process.env.TABLE_NAME,
+            Key:{
+                id: param
+            }
+        };
+        let getresult = await dynamodb.get(getparam).promise();
+        console.log(getresult);
+        result = {
+            success: true,
+            longurl: getresult['long_url']
+        }
+    } catch (error) {
+        console.error(error);
+        result = {
+            success: false,
+            message: 'something went wrong'
+        }
+    }
+}
+
+module.exports.handler = async (event, context) => {
+    let operation = event.requestContext.http.method;
+    let domain = event.requestContext.domainName;
+    let path = event.requestContext.path;
+    console.log('operation : ', operation);
+    console.log('domain : ', domain);
+    console.log('path : ', path);
+    try {
+        if (operation === 'POST') {
+            let body = JSON.parse(event.body);
+            let result = await create_shorturl(body, domain, path);
+            if (result.success) {
+                return {
+                    statusCode : 200,
+                    shorturl: result.shorturl
+                }
+            } else {
+                return {
+                    statusCode: 500,
+                    message: result.message
+                }
+            }
+        }else if (operation === 'GET'){
+            let param = event.pathParameters.short_id
+            let result = await retrieve_long_url(param);
+            if (result.success) {
+                return {
+                    statusCode: 301,
+                    headers:{
+                        Location: result.longurl
+                    }
+                }
+            } else {
+                return {
+                    statusCode: 500,
+                    message: result.message
+                }
+            }
+        }  
+    } catch (error) {
+        console.error(error);
+        return {
+            statusCode: 500,
+            message: 'something went wrong in event handler'
+        }
+    }
+}
